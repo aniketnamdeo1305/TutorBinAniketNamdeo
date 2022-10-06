de6c5d1a2e87a56ffda445d31947688d31d577ad
Aniket Namdeo <aniket.n@coppermobile.com>
2022-10-06 18:33:58 +0530

    All API completed successfully

--- src/models/index.js
+++ src/models/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/tutorbin-assignment', { useNewUrlParser: true }, (err) => {
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }, (err) => {
     if (!err) { console.log('Database Connection Successful!') }
     else { console.log('Error in DB connection : ' + err) }
 });
--- src/routes/todo.route.js
+++ src/routes/todo.route.js
@@ -1,3 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const { todoController } = require('../controllers/index')
+const { validateToken } = require("../middlewares/validateToken");
+
+router.get('/list', validateToken, todoController.listTasks);
+
+router.post('/add', validateToken, todoController.addTask);
+
+router.patch('/update-status', validateToken, todoController.updateTaskStatus);
+
+router.delete('/delete', validateToken, todoController.deleteTask);
+
+router.put('/edit', validateToken, todoController.editTask);
+
+router.get('/getTask', validateToken, todoController.getTask);
+
+
 module.exports = router;
